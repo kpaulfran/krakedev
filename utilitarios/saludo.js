f33f7d57782a3db441c4b6ad9f1c5fea4cfac4ab
kpaulfran <kpaulfran23@gmail.com>
2026-01-11 21:13:40 -0500

    funcion recuperar Int y Float

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -5,9 +5,25 @@ recuperarTexto = function(idComponente){
     valorIngresado = componente.ariaValueMax;
 }
 
+recuperarInt = function(idComponente){
+    let valorCaja = recuperarTexto(idComponente);
+    let valorEntero = parseInt(valorCaja);
+    return valorEntero;
+}
+
+recuperarFloat = function(idComponente){
+    let valorCaja = recuperarTexto(idComponente);
+    let valorFlotante = parseFloat(valorCaja);
+    return valorFlotante;
+}
+
 saludar = function(){
     //recuperar valor de caja de texto
     let nombre = recuperarTexto("txtNombre");
     //recuperar el apellido
     let apellido = recuperarTexto("txtApellido");
+    //recuperar edad
+    let edad = recuperarInt("txtEdad");
+    //recuperar estatura
+    let estatura = recuperarFloat("txtEstatura");
 }
